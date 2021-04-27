93275f65116607aae249c1841b29655041e4c8bb
Klyavlin O.V <klyavlinov@uvd.chel.su>
2021-04-27 11:09:50 +0500

    build: add base class Frame

--- Bowling.Domain/Frame.cs
+++ Bowling.Domain/Frame.cs
@@ -6,5 +6,32 @@ namespace Bowling.Domain
 {
     class Frame
     {
+        private int _pinsFirstThrow;
+        private int _pinsSecondThrow;
+        private int _score;
+        public int FirstThrow()
+        {
+            return _pinsFirstThrow;
+        }
+        public int SecondThrow()
+        {
+            return _pinsSecondThrow;
+        }
+        public Frame Add(int first, int second)
+        {
+            if (first == 10)
+                return new Frame();//strike
+            if ((first + second) == 10)
+                return new Frame();//spare
+            return new Frame();//open
+        }
+        public int Score()
+        {
+            return _pinsFirstThrow + _pinsSecondThrow + _score;
+        }
+        public void Bonus(int points)
+        {
+            _score += points;
+        }
     }
 }
--- Bowling.Test/UnitTest1.cs
+++ Bowling.Test/UnitTest1.cs
@@ -1,6 +1,8 @@
 using System;
 using Xunit;
 
+//
+
 namespace Bowling.Test
 {
     public class UnitTest1
